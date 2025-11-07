6172dd37aef9d2aa5ed4b7daaa178ab13ab7063b
Kevin McDermott <bigkevmcd@gmail.com>
2025-11-07 16:00:09 +0000

    Switch to .spec.github for GitHub Provider config
    This moves the GithubConfig into a field within .spec - creating the
    secret in the setup phase, and not in the saving phase.
    
    This doesn't fully work because the struct that receives the data is not
    defaulting the TLS to true.

--- pkg/auth/providers/github/githubconfig_actions.go
+++ pkg/auth/providers/github/githubconfig_actions.go
@@ -10,9 +10,10 @@ import (
 	"github.com/rancher/norman/httperror"
 	"github.com/rancher/norman/types"
 	"github.com/rancher/norman/types/convert"
-	v32 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	apiv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/auth/providers/common"
 	client "github.com/rancher/rancher/pkg/client/generated/management/v3"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/util/retry"
 )
 
@@ -42,8 +43,8 @@ func (g *Provider) actionHandler(actionName string, action *types.Action, reques
 }
 
 func (g *Provider) configureTest(request *types.APIContext) error {
-	githubConfig := &v32.GithubConfig{}
-	if err := json.NewDecoder(request.Request.Body).Decode(githubConfig); err != nil {
+	githubConfig := apiv3.GithubConfig{}
+	if err := json.NewDecoder(request.Request.Body).Decode(&githubConfig); err != nil {
 		return httperror.NewAPIError(httperror.InvalidBodyContent,
 			fmt.Sprintf("Failed to parse body: %v", err))
 	}
@@ -58,20 +59,97 @@ func (g *Provider) configureTest(request *types.APIContext) error {
 	return nil
 }
 
-func formGithubRedirectURL(githubConfig *v32.GithubConfig) string {
-	return githubRedirectURL(githubConfig.Hostname, githubConfig.ClientID, githubConfig.TLS)
+func (g *Provider) testAndApply(request *types.APIContext) error {
+	githubConfigApplyInput := &apiv3.GithubConfigApplyInput{}
+
+	if err := json.NewDecoder(request.Request.Body).Decode(githubConfigApplyInput); err != nil {
+		return httperror.NewAPIError(httperror.InvalidBodyContent,
+			fmt.Sprintf("Failed to parse body: %v", err))
+	}
+
+	// Create an AuthConfig from the input
+	authConfig := &apiv3.AuthConfig{
+		Spec: apiv3.AuthConfigSpec{
+			Github: &apiv3.GithubConfig{
+				Hostname: githubConfigApplyInput.GithubConfig.Hostname,
+				// TODO: Investigate why this is not defaulting to true
+				TLS:                 true,
+				ClientID:            githubConfigApplyInput.GithubConfig.ClientID,
+				AdditionalClientIDs: githubConfigApplyInput.GithubConfig.AdditionalClientIDs,
+				HostnameToClientID:  githubConfigApplyInput.GithubConfig.HostnameToClientID,
+			},
+		},
+		Enabled: githubConfigApplyInput.Enabled,
+	}
+
+	githubLogin := &apiv3.GithubLogin{
+		Code: githubConfigApplyInput.Code,
+	}
+
+	// Call provider to testLogin
+	userPrincipal, groupPrincipals, providerInfo, err := g.LoginUser(
+		"", githubLogin, authConfig, true, githubConfigApplyInput.GithubConfig.ClientSecret)
+	if err != nil {
+		if httperror.IsAPIError(err) {
+			return err
+		}
+		return errors.Wrap(err, "server error while authenticating")
+	}
+
+	// if this works, save githubConfig CR adding enabled flag
+	user, err := g.userMGR.SetPrincipalOnCurrentUser(request.Request, userPrincipal)
+	if err != nil {
+		return err
+	}
+
+	field := strings.ToLower(apiv3.GithubConfigFieldClientSecret)
+	name, err := common.CreateOrUpdateSecrets(g.secrets, convert.ToString(githubConfigApplyInput.GithubConfig.ClientSecret), field, strings.ToLower(Name))
+	if err != nil {
+		// TODO: Improve this error
+		return err
+	}
+	authConfig.Spec.Github.ClientSecretRef = secretRefFromName(name)
+	authConfig.Enabled = githubConfigApplyInput.Enabled
+
+	if err := g.saveGithubConfig(authConfig); err != nil {
+		return httperror.NewAPIError(httperror.ServerError, fmt.Sprintf("Failed to save github config: %v", err))
+	}
+
+	userExtraInfo := g.GetUserExtraAttributes(userPrincipal)
+	if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		return g.userMGR.UserAttributeCreateOrUpdate(user.Name, userPrincipal.Provider, groupPrincipals, userExtraInfo)
+	}); err != nil {
+		return httperror.NewAPIError(httperror.ServerError, fmt.Sprintf("Failed to create or update userAttribute: %v", err))
+	}
+
+	return g.tokenMGR.CreateTokenAndSetCookie(user.Name, userPrincipal, groupPrincipals, providerInfo, 0, "Token via Github Configuration", request)
+}
+
+func secretRefFromName(s string) *apiv3.SecretReference {
+	parts := strings.SplitN(s, ":", 2)
+
+	return &apiv3.SecretReference{
+		Namespace: parts[0],
+		Name:      parts[1],
+	}
+}
+
+func formGithubRedirectURL(gc apiv3.GithubConfig) string {
+	// Default tls to true because it's not being defaulted.
+	return githubRedirectURL(gc.Hostname, gc.ClientID, true)
 }
 
 func formGithubRedirectURLFromMap(config map[string]any) string {
-	hostname, _ := config[client.GithubConfigFieldHostname].(string)
-	clientID, _ := config[client.GithubConfigFieldClientID].(string)
-	tls, _ := config[client.GithubConfigFieldTLS].(bool)
+	hostname, _, _ := unstructured.NestedString(config, "spec", "github", client.GithubConfigFieldHostname)
+	clientID, _, _ := unstructured.NestedString(config, "spec", "github", client.GithubConfigFieldClientID)
+	tls, _, _ := unstructured.NestedBool(config, "spec", "github", client.GithubConfigFieldTLS)
 
 	requestHostname := convert.ToString(config[".host"])
 	clientIDs := convert.ToMapInterface(config["hostnameToClientId"])
 	if otherID, ok := clientIDs[requestHostname]; ok {
 		clientID = convert.ToString(otherID)
 	}
+
 	return githubRedirectURL(hostname, clientID, tls)
 }
 
@@ -86,59 +164,7 @@ func githubRedirectURL(hostname, clientID string, tls bool) string {
 	} else {
 		redirect = githubDefaultHostName
 	}
+
 	redirect = redirect + "/login/oauth/authorize?client_id=" + clientID
 	return redirect
-}
-
-func (g *Provider) testAndApply(request *types.APIContext) error {
-	var githubConfig v32.GithubConfig
-	githubConfigApplyInput := &v32.GithubConfigApplyInput{}
-
-	if err := json.NewDecoder(request.Request.Body).Decode(githubConfigApplyInput); err != nil {
-		return httperror.NewAPIError(httperror.InvalidBodyContent,
-			fmt.Sprintf("Failed to parse body: %v", err))
-	}
-	githubConfig = githubConfigApplyInput.GithubConfig
-	githubLogin := &v32.GithubLogin{
-		Code: githubConfigApplyInput.Code,
-	}
-
-	if githubConfig.ClientSecret != "" {
-		value, err := common.ReadFromSecret(g.secrets, githubConfig.ClientSecret,
-			strings.ToLower(client.GithubConfigFieldClientSecret))
-		if err != nil {
-			return err
-		}
-		githubConfig.ClientSecret = value
-	}
-
-	// Call provider to testLogin
-	userPrincipal, groupPrincipals, providerInfo, err := g.LoginUser("", githubLogin, &githubConfig, true)
-	if err != nil {
-		if httperror.IsAPIError(err) {
-			return err
-		}
-		return errors.Wrap(err, "server error while authenticating")
-	}
-
-	// if this works, save githubConfig CR adding enabled flag
-	user, err := g.userMGR.SetPrincipalOnCurrentUser(request.Request, userPrincipal)
-	if err != nil {
-		return err
-	}
-
-	githubConfig.Enabled = githubConfigApplyInput.Enabled
-	err = g.saveGithubConfig(&githubConfig)
-	if err != nil {
-		return httperror.NewAPIError(httperror.ServerError, fmt.Sprintf("Failed to save github config: %v", err))
-	}
-
-	userExtraInfo := g.GetUserExtraAttributes(userPrincipal)
-	if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		return g.userMGR.UserAttributeCreateOrUpdate(user.Name, userPrincipal.Provider, groupPrincipals, userExtraInfo)
-	}); err != nil {
-		return httperror.NewAPIError(httperror.ServerError, fmt.Sprintf("Failed to create or update userAttribute: %v", err))
-	}
-
-	return g.tokenMGR.CreateTokenAndSetCookie(user.Name, userPrincipal, groupPrincipals, providerInfo, 0, "Token via Github Configuration", request)
 }
