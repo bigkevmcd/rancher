fa62c9cf2863d60640e6762ca48cebe69946d379
Kevin McDermott <bigkevmcd@gmail.com>
2026-02-19 08:55:39 +0000

    Use DeleteCollection to simplify the SCIM cleanup.

--- pkg/auth/providers/scim/cleanup.go
+++ pkg/auth/providers/scim/cleanup.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	wcorev1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 )
@@ -15,16 +14,8 @@ func CleanupSecrets(secrets wcorev1.SecretController, provider string) error {
 		authProviderLabel: provider,
 	}
 
-	list, err := secrets.List(tokenSecretNamespace, metav1.ListOptions{LabelSelector: labelSet.AsSelector().String()})
-	if err != nil {
+	if err := secrets.DeleteCollection(tokenSecretNamespace, metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: labelSet.AsSelector().String()}); err != nil {
 		return fmt.Errorf("scim::Cleanup: failed to list token secrets for provider %s: %w", provider, err)
-	}
-
-	for _, secret := range list.Items {
-		err = secrets.Delete(secret.Namespace, secret.Name, &metav1.DeleteOptions{})
-		if err != nil && !apierrors.IsNotFound(err) {
-			return fmt.Errorf("scim::Cleanup: failed to delete token secret %s for provider %s: %w", secret.Name, provider, err)
-		}
 	}
 
 	return nil
